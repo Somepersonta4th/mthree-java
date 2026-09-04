9713532a23f3fba28a0be9472e54109817b98de8
Somepersonthe4th <Somepersonthe4th@gmail.com>
2026-09-04 14:23:41 +0100

    updated comments

--- Coding/src/com/mthree/academy/c458/team3/SeanMeaney/InterestCalculator/InterestCalculator.java
+++ Coding/src/com/mthree/academy/c458/team3/SeanMeaney/InterestCalculator/InterestCalculator.java
@@ -1,4 +1,31 @@
 package com.mthree.academy.c458.team3.SeanMeaney.InterestCalculator;
 
+import java.util.Scanner;
+
 public class InterestCalculator {
+    public static void main(String[] args) {
+
+        double annualInterestRate;
+        double currentBalance;
+        int years;
+        var s = new Scanner(System.in);
+
+        //get inputs
+        System.out.println("annual interest rate %:");
+        annualInterestRate = Double.parseDouble(s.nextLine());
+        System.out.println("initial amount of principal:");
+        currentBalance = Double.parseDouble(s.nextLine());
+        System.out.println("number of years the money is to stay in the fund:");
+        years = Integer.parseInt(s.nextLine());
+
+        //iterate for each year and print output
+        for (int i = 0; i < years; i++) {
+            System.out.println("Year "+i+":\n" +
+                "Began with $" + currentBalance);
+            double change = currentBalance * (annualInterestRate / 400);
+            currentBalance+= change;
+            System.out.println("Earned $"+ change +"\n" +
+                    "Ended with $"+currentBalance);
+        }
+    }
 }
--- Coding/src/com/mthree/academy/c458/team3/SeanMeaney/LuckySevens/LuckySevens.java
+++ Coding/src/com/mthree/academy/c458/team3/SeanMeaney/LuckySevens/LuckySevens.java
@@ -1,4 +1,37 @@
 package com.mthree.academy.c458.team3.SeanMeaney.LuckySevens;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class LuckySevens {
+    public static void main(String[] args) {
+        var s = new Scanner(System.in);
+        var RNG  = new Random();
+        int rolls = 0;
+        int maxRolls = 0;
+
+        System.out.println("How many dollars do you bet?");
+        int dollars = Integer.parseInt(s.nextLine());
+
+        int maxDollars = dollars;
+
+        while (dollars > 0) {
+            rolls++;
+            int sum = RNG.nextInt(1,7) + RNG.nextInt(1,7);
+
+            if (sum == 7) { //win
+                dollars += 4;
+                if (dollars >= maxDollars) { //track max
+                    maxDollars = dollars;
+                    maxRolls = rolls;
+                }
+            } else { //lose
+                dollars--;
+            }
+        }
+
+        System.out.println("You are broke after " + rolls + " rolls.");
+        System.out.println("You should have quit after "+maxRolls+" rolls when you had $"+maxDollars+".");
+
+    }
 }

520855672e1b3aafa89e125ae8fbb5669a343c73
Somepersonthe4th <Somepersonthe4th@gmail.com>
2026-09-08 15:05:00 +0100

    simple calculator added

---
+++ Coding/src/com/mthree/academy/c458/team3/SeanMeaney/ClassesAndObjects/ClassModeling/ClassModeling.java
@@ -0,0 +1,7 @@
+package com.mthree.academy.c458.team3.SeanMeaney.ClassesAndObjects;
+
+public class ClassModeling {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Coding/src/com/mthree/academy/c458/team3/SeanMeaney/ClassesAndObjects/SimpleCalculator/SimpleCalculator.java
@@ -0,0 +1,4 @@
+package com.mthree.academy.c458.team3.SeanMeaney.ClassesAndObjects.SimpleCalculator;
+
+public class SimpleCalculator {
+}

337d8e0ee2e334a1e4af2a2aeede820b389f773b
Somepersonthe4th <Somepersonthe4th@gmail.com>
2026-09-10 17:02:14 +0100

    defined UserIO interface. implemented placeholder implementation

--- Coding/src/com/mthree/academy/c458/team3/SeanMeaney/ClassesAndObjects/ClassRoster/ui/UserIO.java
+++ Coding/src/com/mthree/academy/c458/team3/SeanMeaney/ClassesAndObjects/ClassRoster/ui/UserIO.java
@@ -2,4 +2,26 @@ package com.mthree.academy.c458.team3.SeanMeaney.ClassesAndObjects.ClassRoster.u
 
 //defines the methods that must be implemented to interact with the user
 public interface UserIO {
+
+    void print(String message);
+
+    String readString(String prompt);
+
+    double readDouble(String prompt);
+
+    double readDouble(String prompt, double min, double max);
+
+    float readFloat(String prompt);
+
+    float readFloat(String prompt,float min, float max);
+
+    float readInt(String prompt);
+
+    float readInt(String prompt,int min, int max);
+
+    float readLong(String prompt);
+
+    float readLong(String prompt,long min, long max);
+
+
 }
--- Coding/src/com/mthree/academy/c458/team3/SeanMeaney/ClassesAndObjects/ClassRoster/ui/UserIOConsoleImpl.java
+++ Coding/src/com/mthree/academy/c458/team3/SeanMeaney/ClassesAndObjects/ClassRoster/ui/UserIOConsoleImpl.java
@@ -2,4 +2,54 @@ package com.mthree.academy.c458.team3.SeanMeaney.ClassesAndObjects.ClassRoster.u
 
 //console-specific implementation of UserIO
 public class UserIOConsoleImpl implements UserIO {
+
+    @Override
+    public void print(String message) {
+
+    }
+
+    @Override
+    public String readString(String prompt) {
+        return "";
+    }
+
+    @Override
+    public double readDouble(String prompt) {
+        return 0;
+    }
+
+    @Override
+    public double readDouble(String prompt, double min, double max) {
+        return 0;
+    }
+
+    @Override
+    public float readFloat(String prompt) {
+        return 0;
+    }
+
+    @Override
+    public float readFloat(String prompt, float min, float max) {
+        return 0;
+    }
+
+    @Override
+    public float readInt(String prompt) {
+        return 0;
+    }
+
+    @Override
+    public float readInt(String prompt, int min, int max) {
+        return 0;
+    }
+
+    @Override
+    public float readLong(String prompt) {
+        return 0;
+    }
+
+    @Override
+    public float readLong(String prompt, long min, long max) {
+        return 0;
+    }
 }

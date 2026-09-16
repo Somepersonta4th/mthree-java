6ae79ce2bab8de176f55e7f923e17db6671f3026
Somepersonthe4th <Somepersonthe4th@gmail.com>
2026-09-16 18:10:33 +0100

    implemented tax dto

--- Assignments/FlooringMastery/src/main/java/org/WileyEdgeCorp/FlooringMastery/dto/Tax.java
+++ Assignments/FlooringMastery/src/main/java/org/WileyEdgeCorp/FlooringMastery/dto/Tax.java
@@ -1,4 +1,21 @@
 package org.WileyEdgeCorp.FlooringMastery.dto;
 
+import java.math.BigDecimal;
+
 public class Tax {
+    private String state;
+    private String stateAbbreviation;
+    private BigDecimal taxRate;
+
+    public String getState() {
+        return state;
+    }
+
+    public String getStateAbbreviation() {
+        return stateAbbreviation;
+    }
+
+    public BigDecimal getTaxRate() {
+        return taxRate;
+    }
 }

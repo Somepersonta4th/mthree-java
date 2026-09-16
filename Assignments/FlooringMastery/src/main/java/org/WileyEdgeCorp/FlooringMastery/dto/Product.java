333d16e63509b80751415743f043e58d2cf3f406
Somepersonthe4th <Somepersonthe4th@gmail.com>
2026-09-16 18:08:06 +0100

    implemented product dto

--- Assignments/FlooringMastery/src/main/java/org/WileyEdgeCorp/FlooringMastery/dto/Product.java
+++ Assignments/FlooringMastery/src/main/java/org/WileyEdgeCorp/FlooringMastery/dto/Product.java
@@ -1,4 +1,33 @@
 package org.WileyEdgeCorp.FlooringMastery.dto;
 
+import java.math.BigDecimal;
+
 public class Product {
+    private String productType;
+    private BigDecimal costPerSquareFoot;
+    private BigDecimal labourCostPerSquareFoot;
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public BigDecimal getCostPerSquareFoot() {
+        return costPerSquareFoot;
+    }
+
+    public void setCostPerSquareFoot(BigDecimal costPerSquareFoot) {
+        this.costPerSquareFoot = costPerSquareFoot;
+    }
+
+    public BigDecimal getLabourCostPerSquareFoot() {
+        return labourCostPerSquareFoot;
+    }
+
+    public void setLabourCostPerSquareFoot(BigDecimal labourCostPerSquareFoot) {
+        this.labourCostPerSquareFoot = labourCostPerSquareFoot;
+    }
 }

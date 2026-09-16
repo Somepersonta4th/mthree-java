5f9fb46c1108087b9813a4ac0c999540dbf36109
Somepersonthe4th <Somepersonthe4th@gmail.com>
2026-09-16 17:45:44 +0100

    added file impl

--- Coding/src/com/mthree/academy/c458/team3/SeanMeaney/IntermediateJava/java/VendingMachine/dao/VendingDao.java
+++ Coding/src/com/mthree/academy/c458/team3/SeanMeaney/IntermediateJava/java/VendingMachine/dao/VendingDao.java
@@ -2,8 +2,12 @@ package dao;
 
 import dto.VendingItem;
 
+import java.util.ArrayList;
+
 public interface VendingDao {
 
-    public VendingItem getItem();
+    public VendingItem getItem(String name);
+
+    public ArrayList<VendingItem> getAllItem();
 
 }
--- Coding/src/com/mthree/academy/c458/team3/SeanMeaney/IntermediateJava/java/VendingMachine/dao/VendingDaoFileImpl.java
+++ Coding/src/com/mthree/academy/c458/team3/SeanMeaney/IntermediateJava/java/VendingMachine/dao/VendingDaoFileImpl.java
@@ -1,4 +1,23 @@
 package dao;
 
+import dto.VendingItem;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class VendingDaoFileImpl implements VendingDao {
+
+    private HashMap<String,VendingItem> items;
+
+
+
+    @Override
+    public VendingItem getItem(String name) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<VendingItem> getAllItem() {
+        return null;
+    }
 }

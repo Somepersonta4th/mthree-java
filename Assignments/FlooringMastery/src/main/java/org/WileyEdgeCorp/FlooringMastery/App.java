4ffd186aa5e162e862af96b1fb776ae3fdffc27a
Somepersonthe4th <Somepersonthe4th@gmail.com>
2026-09-16 14:15:46 +0100

    Added flooring mastery assignment

---
+++ Assignments/FlooringMastery/src/main/java/org/WileyEdgeCorp/FlooringMastery/App.java
@@ -0,0 +1,16 @@
+/* notes
+cannot end prematurely
+minimum 100 square feet
+cannot do business in a state with no tax record
+display order at end to confirm
+edit checks for each field, displays current value, keeps current value if empty input
+confirm on remove
+ */
+
+package org.WileyEdgeCorp.FlooringMastery;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}

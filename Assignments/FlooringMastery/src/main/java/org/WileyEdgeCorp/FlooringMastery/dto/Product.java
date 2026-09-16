3d45b9f226c5589115cc7941739fd701a4b96800
Somepersonthe4th <Somepersonthe4th@gmail.com>
2026-09-16 18:10:06 +0100

    removed setters

--- Assignments/FlooringMastery/src/main/java/org/WileyEdgeCorp/FlooringMastery/dto/Product.java
+++ Assignments/FlooringMastery/src/main/java/org/WileyEdgeCorp/FlooringMastery/dto/Product.java
@@ -11,23 +11,11 @@ public class Product {
         return productType;
     }
 
-    public void setProductType(String productType) {
-        this.productType = productType;
-    }
-
     public BigDecimal getCostPerSquareFoot() {
         return costPerSquareFoot;
     }
 
-    public void setCostPerSquareFoot(BigDecimal costPerSquareFoot) {
-        this.costPerSquareFoot = costPerSquareFoot;
-    }
-
     public BigDecimal getLabourCostPerSquareFoot() {
         return labourCostPerSquareFoot;
-    }
-
-    public void setLabourCostPerSquareFoot(BigDecimal labourCostPerSquareFoot) {
-        this.labourCostPerSquareFoot = labourCostPerSquareFoot;
     }
 }

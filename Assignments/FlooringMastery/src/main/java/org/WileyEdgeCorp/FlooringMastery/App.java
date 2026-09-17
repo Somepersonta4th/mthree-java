9ea57affdc70733e20cc3d651f75989b68801ddf
Somepersonthe4th <Somepersonthe4th@gmail.com>
2026-09-17 15:14:21 +0100

    changed note

--- Assignments/FlooringMastery/src/main/java/org/WileyEdgeCorp/FlooringMastery/App.java
+++ Assignments/FlooringMastery/src/main/java/org/WileyEdgeCorp/FlooringMastery/App.java
@@ -5,6 +5,7 @@ cannot do business in a state with no tax record
 display order at end to confirm
 edit checks for each field, displays current value, keeps current value if empty input
 confirm on remove
+use lambdas
  */
 
 package org.WileyEdgeCorp.FlooringMastery;
